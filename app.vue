abee5b3b7811c89b23ca52b58871251f6f76f4f1
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-04 20:28:20 -0300

    chore: add setup config

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: () => "Gerenciador de tarefas",
+  htmlAttrs: {
+    lang: "pt-br",
+  },
+});
+</script>
+
 <template>
   <div>app</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-svg", href: "/favicon.svg" }],
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire", "nuxt-icon"],
   vuefire: {
     config: {
       apiKey: "...",
