8d31c6ceda42033b5d24556c1c1f73f3613fe156
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-05 07:44:17 -0300

    chore: remove link to forgot password

--- components/auth/form/SignIn.vue
+++ components/auth/form/SignIn.vue
@@ -99,13 +99,6 @@ const goToRegister = () => {
             />
             <VErrorMessage name="password" class="text-red" />
           </div>
-          <div class="flex items-center justify-end">
-            <nuxt-link
-              to="/auth"
-              class="text-sm font-medium text-primary-600 hover:underline"
-              >Esqueceu sua senha?</nuxt-link
-            >
-          </div>
           <button
             type="submit"
             class="w-full text-white bg-purple hover:brightness-125 transition-colors focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
