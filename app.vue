e4914833534f4f1487c8c444b24bbfc9716b2d16
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-04 21:33:54 -0300

    style: config flowbite

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
 useHead({
   titleTemplate: () => "Gerenciador de tarefas",
   htmlAttrs: {
     lang: "pt-br",
   },
+});
+
+onMounted(() => {
+  initFlowbite();
 });
 </script>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -40,5 +47,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
