68c9be5c012da71f1b1794a536939ffdcb47b708
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-04 21:25:57 -0300

    chore: add pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,43 @@ export default defineNuxtConfig({
     },
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-vuefire",
+    "nuxt-icon",
+    "@vite-pwa/nuxt",
+  ],
   vuefire: {
     config: {
       apiKey: "...",
       authDomain: "...",
       projectId: "...",
       appId: "...",
+    },
+  },
+  pwa: {
+    manifest: {
+      name: "Gerenciador de tarefas",
+      short_name: "Tarefas",
+      theme_color: "#F5F5F5",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "gerenciador de tarefas",
+        },
+      ],
     },
   },
 });
