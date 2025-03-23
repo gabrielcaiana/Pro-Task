6beba2f641c88078ca1570a832522c4e62848bf1
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-03-23 16:56:25 -0300

    refactor: migrate to nuxt 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    "./app/components/**/*.{js,vue,ts}",
+    "./app./layouts/**/*.vue",
+    "./app./pages/**/*.vue",
+    "./app./plugins/**/*.{js,ts}",
+    "./app./nuxt.config.{js,ts}",
     "./node_modules/flowbite/**/*.{js,ts}",
   ],
   theme: {
